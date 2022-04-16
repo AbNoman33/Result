6516077a6f5b1912f3dfb389298a3da2ebf517a5
Noman <abnoman330@gmail.com>
2022-04-17 01:29:57 +0600

    This is for Result

---
+++ script.js
@@ -0,0 +1,20 @@
+let s_name = prompt("Name Please: ");
+let age = prompt("Age Please");
+let roll = prompt("Roll Number:");
+let bangla = prompt("Bangla");
+let english = prompt("English");
+let math = prompt("Math");
+let religion = prompt("Religion");
+
+
+console.log(`
+     Name: ${s_name}
+     Age: ${age}
+     Roll: ${roll}
+
+     Subject    Marks
+     
+     Bangla    ${bangla}
+     English   ${english}
+     Math      ${math}
+     Religion  ${religion}    `);
